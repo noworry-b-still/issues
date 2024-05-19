161056c478e2bb1d5494a372df573ae1144322a6
lord-cosmos <teslaatozinspires7@gmail.com>
2024-05-19 14:32:21 -0400

    sort, pick count issues

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -29,6 +29,7 @@ defmodule Issues.CLI do
             |> args_to_internal_representation
   end
 
+  def args_to_internal_representation([user, project, count ]) when is_binary(count), do: {user, project, String.to_integer(count)}
   def args_to_internal_representation([user, project, count ]), do: {user, project, count}
   def args_to_internal_representation([user, project]), do: {user, project, @default_count}
   def args_to_internal_representation(_), do: :help
@@ -41,7 +42,29 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process([user, project, _count ]) do
+  def process([user, project, count ]) do
     Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> sort_into_descending_order
+    |> last(count)
+  end
+
+  def last(list,count) do
+    list |> Enum.take(count) |> Enum.reverse
+  end
+
+  def decode_response({:ok, body}) do
+    body
+  end
+
+  def decode_response({:error, error}) do
+    IO.puts "Error fetching from Github: #{error["message"]}"
+    System.halt(2)
+  end
+
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn issue1, issue2 -> issue1["created_at"] >= issue2["created_at"] end)
   end
 end
