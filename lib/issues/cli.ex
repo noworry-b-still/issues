9a1c18238ee5673b69fd2d1c63f4708d07cf4378
lord-cosmos <teslaatozinspires7@gmail.com>
2024-05-18 22:22:35 -0400

    refactor code - remove conditional logic

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -23,15 +23,13 @@ defmodule Issues.CLI do
   """
 
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [ help: :boolean ] , aliases: [ h: :help])
-
-    case parse do
-      {[help: true], _, _} -> :help
-      {_, [ user, project, count ], _ } -> {user, project, String.to_integer(count)}
-      {_, [ user, project ], _} -> {user, project, @default_count}
-      _ -> :help
-    end
-
+    OptionParser.parse(argv, switches: [ help: :boolean ] , aliases: [ h: :help])
+            |> elem(1)
+            |> args_to_internal_representation
   end
+
+  def args_to_internal_representation([user, project, count ]), do: {user, project, count}
+  def args_to_internal_representation([user, project]), do: {user, project, @default_count}
+  def args_to_internal_representation(_), do: :help
 
 end
