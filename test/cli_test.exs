64b4f9d60f63022a9f0cea3b03f323f39ea31d55
lord-cosmos <teslaatozinspires7@gmail.com>
2024-05-18 22:16:43 -0400

    add ExUnit test cases for CLI.parse_args

---
+++ test/cli_test.exs
@@ -0,0 +1,20 @@
+defmodule CliTest do
+  use ExUnit.Case
+  doctest Issues
+
+  import Issues.CLI, only: [parse_args: 1]
+
+  test ":help returned by option parsing with -h and --help options" do
+    assert parse_args(["-h", "anything"]) == :help
+    assert parse_args(["--help", "anything2"]) == :help
+  end
+
+  test "check three values are returned if three args are given" do
+    assert parse_args(["user", "project", "99"]) == {"user", "project" , 99}
+  end
+
+  test "check three values are returned if only 2 args are given" do
+    assert parse_args(["user", "project"]) == {"user", "project", 4}
+  end
+
+end
