e7b6ca12a6a32c7a425ef52422e478982d6bc891
lord-cosmos <teslaatozinspires7@gmail.com>
2024-05-19 16:58:58 -0400

    config env var not working, retract to hardcoding URL

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,27 +1,28 @@
 defmodule Issues.GithubIssues do
-  @user_agent [{"User-agent", "Elixir elixirdev@google.com"}]
+  @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
+
+  # Use a module attribute to fetch the value at compile time
+  # @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
-      issues_url(user, project)
-      |> HTTPoison.get(@user_agent)
-      |> handle_response
+    issues_url(user, project)
+    |> HTTPoison.get()
+    |> handle_response()
   end
 
-  @github_url Application.get_env(:issues, :github_url)
   def issues_url(user, project) do
-    "#{@github_url}/repos/#{user}/#{project}/issues"
+    "https://api.github.com/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({_ , %{status_code: status_code, body: body}}) do
-    {
-      status_code |> check_for_error,
-      body        |> Poison.Parser.parse!()
-    }
+  def handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+    {:ok, Poison.Parser.parse!(body)}
   end
 
-  defp check_for_error(200), do: :ok
-  defp check_for_error(_), do: :error
+  def handle_response({:ok, %HTTPoison.Response{status_code: status_code, body: body}}) do
+    {:error, "HTTP Error #{status_code}: #{body}"}
+  end
 
-
+  def handle_response({:error, %HTTPoison.Error{reason: reason}}) do
+    {:error, "HTTPoison Error: #{reason}"}
+  end
 end
-
--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.MixProject do
   def project do
     [
       app: :issues,
+      escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
@@ -11,18 +12,23 @@ defmodule Issues.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:httpoison, "~> 1.0.0"},
       { :poison, "~> 3.1" },
+    ]
+  end
+
+
+  defp escript_config() do
+    [
+      main_module: Issues.CLI
     ]
   end
 end
